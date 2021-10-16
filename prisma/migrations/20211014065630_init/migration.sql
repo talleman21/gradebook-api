0a87924c2070e9c244393ed4a3fe43fe5b2d13de
Thomas Alleman <talleman21@gmail.com>
2021-10-15 21:48:38 -0700

    fix: squash migrations

--- prisma/migrations/20211014065630_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `studentIds` on the `Subject` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Subject" DROP COLUMN "studentIds";
