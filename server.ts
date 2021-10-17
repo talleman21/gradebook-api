f6f7bd1d3979480d2fe721dec8ba3bf3acad256d
Thomas Alleman <talleman21@gmail.com>
2021-10-16 23:27:52 -0700

    Add error handling middleware

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express,{Request,Response,NextFunction} from 'express'
 const app = express()
 const PORT = process.env.PORT
 import {student,subject,curriculum,assignment, instructor} from './routes'
@@ -11,6 +11,17 @@ app.use('/student',student)
 app.use('/subject',subject)
 app.use('/curriculum',curriculum)
 app.use('/assignment',assignment)
+
+// error handler
+app.use(function(err:Error & {status:number}, req:Request, res:Response, next:NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.message);
+});
 
 
 
