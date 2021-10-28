056888495a537e013ff5b37ecbdceaa5b3752c72
Thomas Alleman <talleman21@gmail.com>
2021-10-27 23:13:04 -0700

    fix: update function calls

--- routes/curriculum/index.ts
+++ routes/curriculum/index.ts
@@ -7,8 +7,8 @@ import {deleteOne} from './delete'
 
 export const curriculum = express.Router()
 
-curriculum.get('/',(req,res) => getMany(req,res))
-curriculum.get('/:id',(req,res) => getOne(req,res))
-curriculum.post('/',(req,res) => create(req,res))
-curriculum.put('/:id',(req,res) => updateOne(req,res))
-curriculum.delete('/:id',(req,res) => deleteOne(req,res))
+curriculum.get('/',getMany)
+curriculum.get('/:id',getOne)
+curriculum.post('/',create)
+curriculum.put('/:id',updateOne)
+curriculum.delete('/:id',deleteOne)
