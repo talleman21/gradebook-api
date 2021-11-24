d6f8882f0c28b8ac97b486a17ee0a14c1e2d52da
Thomas Alleman <talleman21@gmail.com>
2021-11-23 20:20:49 -0800

    feat: add user types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export * from "./user";
 export * from "./account";
 export * from "./assignment";
 export * from "./student";
---
+++ src/types/user.ts
@@ -0,0 +1,5 @@
+import { User } from "@prisma/client";
+
+export type RawUser = User;
+
+export type UserDTO = User;
